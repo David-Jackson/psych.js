0c3fdce7fba8e9b2cf7d8c4701b79d4cb1498063
David Jackson <jack8679@kettering.edu>
2019-03-08 09:38:14 -0500

    Updated graph with seperate layers for points, lines, shapes, and graph

--- viewer/assets/scripts/graph.js
+++ viewer/assets/scripts/graph.js
@@ -2,6 +2,8 @@ function Graph(_width, _height) {
 	this.width = _width;
 	this.height = _height;
 	this.points = [];
+	this.lines = [];
+	this.shapes = [];
 	
 	this.initProperties = function() {
 
@@ -20,9 +22,18 @@ function Graph(_width, _height) {
 			secondaryLineColor: color(230),
 			secondaryLineStrokeWeight: 1,
 
+			
+			pointRadius: 10,
+
 			pointColor: color(182, 16, 29),
+			shapeColor: color(255, 0, 0, 50),
 			heatingColor: color(182, 16, 29),
-			coolingColor: color(0, 0, 210)
+			coolingColor: color(0, 0, 210),
+
+			lineStroke: color(182, 16, 29),
+			shapeStroke: color(182, 16, 29),
+
+			lineWeight: 2
 		};
 		
 		this.properties.axes = {
@@ -67,6 +78,31 @@ function Graph(_width, _height) {
 	};
 	this.initProperties();
 	this.updateProperties();
+
+	this.layers = {};
+
+	this.initLayers = function() {
+		this.layers = {
+			chart: createGraphics(this.width, this.height),
+			shapes: createGraphics(this.graphWidth, this.graphHeight),
+			lines: createGraphics(this.graphWidth, this.graphHeight),
+			points: createGraphics(this.graphWidth, this.graphHeight)
+		};
+	}
+
+	this.clearLayers = function() {
+		for (var layerName in this.layers) {
+			this.layers[layerName].clear();
+		}
+	};
+
+	this.drawLayers = function() {
+		for (var layerName in this.layers) {
+			image(this.layers[layerName], 0, 0);
+		}
+	};
+
+	this.initLayers();
 	
 	this.moveScale = function(deltaX, deltaY) {
 		this.scale.x.min += deltaX;
@@ -78,6 +114,7 @@ function Graph(_width, _height) {
 	this.resize = function(newWidth, newHeight) {
 		this.width = newWidth;
 		this.height = newHeight;
+		this.initLayers();
 		this.updateProperties();
 		this.draw();
 	}
@@ -98,9 +135,13 @@ function Graph(_width, _height) {
 	
 	this.draw = function() {
 		var start = new Date().getTime();
+		this.clearLayers();
 		this.drawGraph();
 		this.drawLabels();
+		this.drawShapes();
+		this.drawLines();
 		this.drawPoints();
+		this.drawLayers();
 		var end = new Date().getTime();
 		console.log("Drawing the graph took", end - start, "ms");
 	}
@@ -111,12 +152,13 @@ function Graph(_width, _height) {
 		this.drawPrimaryLines();
 		
 	};
-	
+
 	this.drawSecondaryLines = function() {
 
+		let pg = this.layers.chart;
 
-		stroke(this.properties.defaults.secondaryLineColor);
-		strokeWeight(this.properties.defaults.secondaryLineStrokeWeight);
+		pg.stroke(this.properties.defaults.secondaryLineColor);
+		pg.strokeWeight(this.properties.defaults.secondaryLineStrokeWeight);
 		
 		
 		let MIN_DB = this.properties.axes.x.min;
@@ -128,22 +170,28 @@ function Graph(_width, _height) {
 		let p = psych.calculations.PFt(this.properties.elevation);
 		
 		let rh = 100;
+
+		// Secondary Dry Bulb Lines
 		for (let db = MIN_DB; db <= MAX_DB; db++) {
 			if (db % 5 == 0) continue;
 			let W = psych.calculations.WTR(p, db, rh) * 7000;
 			let x = map(db, MIN_DB, MAX_DB, 0, this.graphWidth);
 			let y = map(W, MIN_W, MAX_W, this.graphHeight, 0);
-			line(x, this.graphHeight, x, y);
+			pg.line(x, this.graphHeight, x, y);
 		}
 		
+
+		// Secondary Humidity Ratio Lines
 		for (let W = MIN_W; W <= MAX_W; W = W + 2) {
 			if (W % 10 == 0) continue;
 			let db = psych.calculations.TRW(p, rh, W / 7000);
 			let x = map(db, MIN_DB, MAX_DB, 0, this.graphWidth);
 			let y = map(W, MIN_W, MAX_W, this.graphHeight, 0);
-			line(x, y, this.graphWidth, y);
+			pg.line(x, y, this.graphWidth, y);
 		}
 
+
+		// Secondary Wet Bulb Lines
 		let min_wb = psych.calculations.BTW(p, MIN_DB, MIN_W / 7000);
 		let max_wb = psych.calculations.BTW(p, MAX_DB, MAX_W / 7000);
 		let wbIncrement = 1;
@@ -167,15 +215,18 @@ function Graph(_width, _height) {
 			}
 			let endX = map(endDb, MIN_DB, MAX_DB, 0, this.graphWidth);
 			let endY = map(endW, MIN_W, MAX_W, this.graphHeight, 0);
-			line(startX, startY, endX, endY);
+			pg.line(startX, startY, endX, endY);
 		}
 		
 	};
 	
 	this.drawPrimaryLines = function() {
 		
-		stroke(this.properties.defaults.primaryLineColor);
-		strokeWeight(this.properties.defaults.primaryLineStrokeWeight);
+		let pg = this.layers.chart;
+
+		pg.stroke(this.properties.defaults.primaryLineColor);
+		pg.strokeWeight(this.properties.defaults.primaryLineStrokeWeight);
+		pg.noFill();
 	
 		let MIN_DB = this.properties.axes.x.min;
 		let MAX_DB = this.properties.axes.x.max;
@@ -186,44 +237,51 @@ function Graph(_width, _height) {
 		let p = psych.calculations.PFt(this.properties.elevation);
 		
 		let rh = 100;
+
+		// Primary Dry Bulb Lines
 		for (let db = MIN_DB; db <= MAX_DB; db = db + 5) {
 			let W = psych.calculations.WTR(p, db, rh) * 7000;
 			let x = map(db, MIN_DB, MAX_DB, 0, this.graphWidth);
 			let y = map(W, MIN_W, MAX_W, this.graphHeight, 0);
-			line(x, this.graphHeight, x, y);
+			pg.line(x, this.graphHeight, x, y);
 		}
 		
+		// Primary Humidity Ratio Lines
 		for (let W = MIN_W; W <= MAX_W; W = W + 10) {
 			let db = psych.calculations.TRW(p, rh, W / 7000);
 			let x = map(db, MIN_DB, MAX_DB, 0, this.graphWidth);
 			let y = map(W, MIN_W, MAX_W, this.graphHeight, 0);
-			line(x, y, this.graphWidth, y);
+			pg.line(x, y, this.graphWidth, y);
 		}
 	
+		// Primary Enthalpy Lines
+		// let intersectionLine = graph.helpers.lineFromTwoPoints(createVector(700, 0), createVector(0, 700));
+		// pg.line(700, 0, 0, 700);
+
 		let minH = psych.calculations.HTW(MIN_DB, MIN_W / 7000);
 		let maxH = psych.calculations.HTW(MAX_DB, MAX_W / 7000);
 		let hIncrement = 5;
 		minH += hIncrement - (minH % hIncrement);
 		maxH -= (maxH % hIncrement);
-		push();
 		for (let h = minH; h <= maxH; h = h + hIncrement) {
 			let startDb = psych.calculations.TRH(p, 100, h);
 			let startW = psych.calculations.WTH(startDb, h) * 7000;
 			let startX = map(startDb, MIN_DB, MAX_DB, 0, this.graphWidth);
 			let startY = map(startW, MIN_W, MAX_W, this.graphHeight, 0);
-			let endDb = psych.calculations.TWH(0, h);
+			let endDb = Math.min(psych.calculations.TWH(0, h), MAX_DB);
 			let endX = map(endDb, MIN_DB, MAX_DB, 0, this.graphWidth);
-			if (endX > this.graphWidth) {
-				endDb = this.properties.axes.x.max;
-				endX = map(endDb, MIN_DB, MAX_DB, 0, this.graphWidth);
-			}
 			let endW = psych.calculations.WTH(endDb, h) * 7000;
 			let endY = map(endW, MIN_W, MAX_W, this.graphHeight, 0);
-			line(startX, startY, endX, endY);
-			
-		}
-		pop();
+			pg.line(startX, startY, endX, endY);
 
+			// let line = graph.helpers.lineFromTwoPoints(
+			// 	createVector(startX, startY), createVector(endX, endY));
+			// let pt = line.intersect(intersectionLine);
+			// pg.line(startX, startY, pt.x, pt.y);
+			// console.log(line);
+		}
+
+		// Primary Wet Bulb Lines
 		let min_wb = psych.calculations.BTW(p, MIN_DB, MIN_W / 7000);
 		let max_wb = psych.calculations.BTW(p, MAX_DB, MAX_W / 7000);
 		let wbIncrement = 5;
@@ -246,29 +304,28 @@ function Graph(_width, _height) {
 			}
 			let endX = map(endDb, MIN_DB, MAX_DB, 0, this.graphWidth);
 			let endY = map(endW, MIN_W, MAX_W, this.graphHeight, 0);
-			line(startX, startY, endX, endY);
+			pg.line(startX, startY, endX, endY);
 		}
 
 		
+		// Primary Relative Humidity Lines
 		for (j = 10; j <= 100; j = j + 10) {
-			stroke(this.properties.defaults.primaryLineColor);
-			strokeWeight(this.properties.defaults.primaryLineStrokeWeight);
-			noFill();
-			beginShape();
+			pg.beginShape();
 			for (var i = 0; i < this.graphWidth; i++) {
 				var db = map(i, 0, this.graphWidth, MIN_DB, MAX_DB);
 				var W = psych.calculations.WTR(p, db, j) * 7000;
 				if (W > MAX_W) break;
 				var y = map(W, MIN_W, MAX_W, this.graphHeight, 0);
-				vertex(i, y);
+				pg.vertex(i, y);
 			}
-			endShape();
+			pg.endShape();
 		}
 
 	};
 	
 	this.drawLabels = function() {
 		
+		let pg = this.layers.chart;
 		
 		let MIN_DB = this.properties.axes.x.min;
 		let MAX_DB = this.properties.axes.x.max;
@@ -285,9 +342,9 @@ function Graph(_width, _height) {
 		min_wb += wbIncrement - (min_wb % wbIncrement);
 		max_wb -= (max_wb % wbIncrement);
 		for (let wb = min_wb; wb <= max_wb; wb = wb + wbIncrement) {
-			textSize(12);
-			var txt = "" + wb + ((wb == max_wb - wbIncrement) ? " WET BUILB TEMPERATURE - " + String.fromCharCode(176) + "F": "");
-			var txtWidth = textWidth(txt);
+			pg.textSize(12);
+			var txt = "" + wb + ((wb == max_wb - (2 * wbIncrement)) ? " WET BUILB TEMPERATURE - " + String.fromCharCode(176) + "F": "");
+			var txtWidth = pg.textWidth(txt);
 			var db1 = psych.calculations.TRB(p, 68, wb);
 			var db2 = psych.calculations.TRB(p, 100, wb);
 			var W1 = psych.calculations.WTR(p, db1, 68);
@@ -297,26 +354,28 @@ function Graph(_width, _height) {
 			var y1 = map(W1 * 7000, MIN_W, MAX_W, this.graphHeight, 0);
 			var y2 = map(W2 * 7000, MIN_W, MAX_W, this.graphHeight, 0);
 			var a = atan((y2 - y1)/(x2 - x1));
-			push();
-			noStroke();
-			textAlign(RIGHT,BOTTOM);
-			fill(0);
-			text(txt.split(" ")[0], x2, y2);
-			textAlign(LEFT,CENTER);
-			translate(x1, y1);
-			rotate(a);
-			fill(this.properties.defaults.backgroundColor);
-			rect(0, -6, txtWidth, 12);
-			fill(0);
-			text(txt, 0, 0);
-			pop();
+			with (pg) {
+				push();
+				noStroke();
+				textAlign(RIGHT,BOTTOM);
+				fill(0);
+				text(txt.split(" ")[0], x2, y2);
+				textAlign(LEFT,CENTER);
+				translate(x1, y1);
+				rotate(a);
+				fill(this.properties.defaults.backgroundColor);
+				rect(0, -6, txtWidth, 12);
+				fill(0);
+				text(txt, 0, 0);
+				pop();
+			}
 		}
 
 		// draw RH labels
 		for (var rh = 10; rh < 100; rh = rh + 10) {
-			textSize(12);
+			pg.textSize(12);
 			var txt = rh + "%" + ((rh == 10) ? " RELATIVE HUMIDITY": "");
-			var txtWidth = textWidth(txt);
+			var txtWidth = pg.textWidth(txt);
 			var x1 = 0.475 * this.graphWidth;
 			var x2 = x1 + txtWidth;
 			var db1 = map(x1, 0, this.graphWidth, MIN_DB, MAX_DB);
@@ -326,64 +385,73 @@ function Graph(_width, _height) {
 			var y1 = map(W1 * 7000, MIN_W, MAX_W, this.graphHeight, 0);
 			var y2 = map(W2 * 7000, MIN_W, MAX_W, this.graphHeight, 0);
 			var a = atan((y2 - y1)/(x2 - x1));
-			push();
-			textAlign(LEFT,CENTER);
-			translate(x1, y1);
-			rotate(a);
-			noStroke();
-			fill(this.properties.defaults.backgroundColor);
-			rect(0, -10, txtWidth, 16 + 4);
-			fill(0);
-			text(txt, 0, 0);
-			pop();
+			with (pg) {
+				push();
+				textAlign(LEFT,CENTER);
+				translate(x1, y1);
+				rotate(a);
+				noStroke();
+				fill(this.properties.defaults.backgroundColor);
+				rect(0, -10, txtWidth, 16 + 4);
+				fill(0);
+				text(txt, 0, 0);
+				pop();
+			}
 		}
 		
 		// draw DB labels
-		push();
-		textAlign(CENTER,CENTER);
-		textSize(12);
-		translate(this.graphWidth / 2, this.graphHeight + (this.properties.axes.x.height * 2 / 3));
-		fill(0);
-		noStroke();
-		text("DRY BULB TEMPERATURE - " + String.fromCharCode(176) + "F", 0, 0);
-		pop();
-		
-		
-		for (let db = MIN_DB + 5; db <= MAX_DB; db = db + 5) {
+		with (pg) {
 			push();
-			let x = map(db, MIN_DB, MAX_DB, 0, this.graphWidth);
-			let y = this.graphHeight + (10);
-			translate(x, y);
+			textAlign(CENTER,CENTER);
+			textSize(12);
+			translate(this.graphWidth / 2, this.graphHeight + (this.properties.axes.x.height * 2 / 3));
 			fill(0);
 			noStroke();
-			textAlign(CENTER,CENTER);
-			text(db, 0, 0);
+			text("DRY BULB TEMPERATURE - " + String.fromCharCode(176) + "F", 0, 0);
 			pop();
 		}
 		
 		
+		for (let db = MIN_DB + 5; db <= MAX_DB; db = db + 5) {
+			let x = map(db, MIN_DB, MAX_DB, 0, this.graphWidth);
+			let y = this.graphHeight + (10);
+			with (pg) {
+				push();
+				translate(x, y);
+				fill(0);
+				noStroke();
+				textAlign(CENTER,CENTER);
+				text(db, 0, 0);
+				pop();
+			}
+		}
+		
 		// draw W labels
-		push();
-		textAlign(CENTER,CENTER);
-		textSize(12);
-		translate(this.graphWidth + (this.properties.axes.y.width * 2 / 3), this.graphHeight / 2);
-		rotate(-HALF_PI);
-		fill(0);
-		noStroke();
-		text("HUMIDITY RATIO - GRAINS OF MOISTURE PER POUND OF DRY AIR", 0, 0);
-		pop();
+		with (pg) {
+			push();
+			textAlign(CENTER,CENTER);
+			textSize(12);
+			translate(this.graphWidth + (this.properties.axes.y.width * 2 / 3), this.graphHeight / 2);
+			rotate(-HALF_PI);
+			fill(0);
+			noStroke();
+			text("HUMIDITY RATIO - GRAINS OF MOISTURE PER POUND OF DRY AIR", 0, 0);
+			pop();
+		}
 		
 		
 		for (let W = MIN_W + 10; W <= MAX_W; W = W + 10) {
-			push();
 			let x = this.graphWidth + 5 + (textWidth(W) / 2);
 			let y = map(W, MIN_W, MAX_W, this.graphHeight, 0);
-			translate(x, y);
-			fill(0);
-			noStroke();
-			textAlign(CENTER,CENTER);
-			text(W, 0, 0);
-			pop();
+			with (pg) {
+				push();
+				translate(x, y);
+				fill(0);
+				noStroke();
+				textAlign(CENTER,CENTER);
+				text(W, 0, 0);
+				pop();
+			}
 		}
 	}
 
@@ -401,7 +469,7 @@ function Graph(_width, _height) {
 		}
 	}
 
-	this.addPoint = function(point) {
+	this.calculatePosition = function(point) {
 		let MIN_DB = this.properties.axes.x.min;
 		let MAX_DB = this.properties.axes.x.max;
 		
@@ -409,19 +477,104 @@ function Graph(_width, _height) {
 		let MAX_W = this.properties.axes.y.max;
 		point.properties.graphX = map(point.properties.db, MIN_DB, MAX_DB, 0, this.graphWidth);
 		point.properties.graphY = map(point.properties.W * 7000, MIN_W, MAX_W, this.graphHeight, 0);
+	}
+
+	this.addPoint = function(style, point) {
+		this.calculatePosition(point);
+		point.style = style;
 		this.points.push(point);
-		this.drawPoints();
+	}
+
+	this.addPoints = function(style, ...points) {
+		points.forEach(point => this.addPoint(style, point));
 	}
 
 	this.addPointFromXY = function(x, y) {
-		this.addPoint(this.pointFromXY(x, y));
+		this.addPoint(null, this.pointFromXY(x, y));
+	}
+
+	this.addLine = function(style, points) {
+		console.log("Adding line with", points.length, "points");
+		points.forEach(pt => {
+			if (typeof pt.properties.graphX == "undefined") this.calculatePosition(pt)
+		});
+		this.lines.push({style: style, points: points});
+	}
+
+	this.addShape = function(style, points) {
+		points.forEach(pt => {
+			if (typeof pt.properties.graphX == "undefined") this.calculatePosition(pt)
+		});
+		this.shapes.push({style: style, points: points});
+	}
+
+	this.tryToAssign = function(val, def) {
+		return(typeof val == "undefined") ? def : val;
 	}
 
 	this.drawPoints = function() {
-		fill(this.properties.defaults.pointColor);
-		noStroke();
+		var pg = this.layers.points;
 		this.points.forEach(function(point) {
-			ellipse(point.properties.graphX, point.properties.graphY, 10, 10);
+			point.style = point.style || {};
+			var radius = this.tryToAssign(point.style.radius, this.properties.defaults.pointRadius);
+			var fillColor = this.tryToAssign(point.style.fill, this.properties.defaults.pointColor);
+			var strokeColor = this.tryToAssign(point.style.stroke, this.properties.defaults.pointStroke);
+			var lineWeight = this.tryToAssign(point.style.weight, this.properties.defaults.pointWeight);
+			(fillColor == null) ? pg.noFill() : pg.fill(fillColor);
+			(strokeColor == null) ? pg.noStroke() : pg.stroke(strokeColor);
+			pg.strokeWeight(lineWeight);
+			pg.ellipse(point.properties.graphX, point.properties.graphY, radius, radius);
+		}, this);
+	}
+
+	this.drawLines = function() {
+		var pg = this.layers.lines;
+		this.lines.forEach(function(line) {
+			console.log("Drawing line with", line.points.length, "points");
+			line.style = line.style || {};
+			var radius = this.tryToAssign(line.style.radius, this.properties.defaults.pointRadius);
+			var fillColor = this.tryToAssign(line.style.fill, this.properties.defaults.shapeColor);
+			var strokeColor = this.tryToAssign(line.style.stroke, this.properties.defaults.shapeStroke);
+			var lineWeight = this.tryToAssign(line.style.weight, this.properties.defaults.lineWeight);
+			var drawPoints = radius != null || fillColor != null;
+			(fillColor == null) ? pg.noFill() : pg.fill(fillColor);
+			(strokeColor == null) ? pg.noStroke() : pg.stroke(strokeColor);
+			pg.strokeWeight(lineWeight);
+			for (var i = 0; i < line.points.length; i++) {
+				var p1 = line.points[i];
+				if (i < line.points.length - 1) {
+					// draw line between two points
+					var p2 = line.points[i + 1];
+					pg.line(p1.properties.graphX, p1.properties.graphY, 
+                    	p2.properties.graphX, p2.properties.graphY);
+				}
+				if (drawPoints) {
+					pg.ellipse(p1.properties.graphX, p1.properties.graphY, radius, radius);
+				}
+			}
+		}, this);
+	}
+
+	this.drawShapes = function() {
+		var pg = this.layers.shapes;
+		this.shapes.forEach(function(shape) {
+			shape.style = shape.style || {};
+			var radius = this.tryToAssign(shape.style.radius, null);
+			var fillColor = this.tryToAssign(shape.style.fill, this.properties.defaults.shapeColor);
+			var strokeColor = this.tryToAssign(shape.style.stroke, this.properties.defaults.lineStroke);
+			var lineWeight = this.tryToAssign(shape.style.weight, this.properties.defaults.lineWeight);
+			var drawPoints = radius != null;
+			(fillColor == null) ? pg.noFill() : pg.fill(fillColor);
+			(strokeColor == null) ? pg.noStroke() : pg.stroke(strokeColor);
+			pg.strokeWeight(lineWeight);
+			pg.beginShape();
+			shape.points.forEach(function(pt) {
+				pg.vertex(pt.properties.graphX, pt.properties.graphY);
+				if (drawPoints) {
+					pg.ellipse(pt.properties.graphX, pt.properties.graphY, radius, radius);
+				}
+			});
+			pg.endShape(CLOSE);
 		}, this);
 	}
 
@@ -433,5 +586,35 @@ function Graph(_width, _height) {
 	}
 
 
+	this.helpers = {
+		Line: function(slope, yIntercept) {
+			this.slope = slope;
+			this.yIntercept = yIntercept;
+
+			this.x = function(y) {
+				return (y - this.yIntercept) / this.slope;
+			}
+
+			this.y = function(x) {
+				return (this.slope * x) + this.yIntercept;
+			}
+
+			this.intersect = function(otherLine) {
+				var x = (otherLine.yIntercept - this.yIntercept) / (this.slope - otherLine.slope);
+				var y = this.y(x);
+				return createVector(x, y);
+			}
+
+			return this;
+		},
+		lineFromTwoPoints: function(v1, v2) {
+			var slope = (v1.y - v2.y) / (v1.x - v2.x);
+			return this.lineFromSlopeAndPoint(slope, v1);
+		},
+		lineFromSlopeAndPoint: function(slope, v1) {
+			var yIntercept = v1.y - (slope * v1.x);
+			return new this.Line(slope, yIntercept);
+		}
+	}
 
 }
